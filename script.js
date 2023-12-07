375ec170cea9a3e3571b6e39f05ddf41815d3764
DreamyTZK <i@tzki.cn>
2023-12-07 21:29:46 +0800

    perf: 调整脚本

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let isEnd = false
 let startRow = 2
 const accountList = []
 const sendEmailObj = {}
-
+const deviceId = '72ecc7d42e65a217c1b3b57ddb8e6453b7778f0d5df4018c4b9949c46a329a0c'
+let access_token = ''
 /**
  * 发送邮件
  * @param {string} to 发送邮箱
@@ -15,14 +16,14 @@ function sendEmail(to, text) {
   const mailer = SMTP.login({
     host: "smtp.qiye.aliyun.com", // 邮箱 的SMTP服务器的域名
     port: 465,
-    username: "admin@antmoe.com", // 邮箱地址
-    password: "8EwhAgLdyTrYQqit", // 邮箱的SMTP密码，非密码
+    username: "邮箱账号", // 邮箱地址
+    password: "邮箱密码", // 邮箱的SMTP密码，非密码
     secure: true
   });
   mailer.send({
     text, // 文本
     to, // 收件人
-    from: "阿里云盘签到<admin@antmoe.com>", // 发件人
+    from: "阿里云盘签到<邮箱账号>", // 发件人
     subject: "阿里云盘签到通知-" + data_time, // 主题
   })
 }
@@ -44,7 +45,18 @@ const sendMailHealper = {
   },
 }
 
+function request(url, data) {
+  const headers = {}
+  if (access_token) {
+    headers["Authorization"] = "Bearer " + access_token
+  }
+  if (deviceId) {
+    headers["x-device-id"] = deviceId
+  }
+  const result = HTTP.post(url, data, { headers })
 
+  return result
+}
 
 function sleep(d) {
   for (var t = Date.now(); Date.now() - t <= d;);
@@ -69,16 +81,15 @@ function getRow(number) {
  * @returns { { accessToken:string | '',phone: string } }
  */
 function getAccessToken(token) {
-  // 发起网络请求-获取token
-  const data = HTTP.post("https://auth.aliyundrive.com/v2/account/token",
-    JSON.stringify({
-      "grant_type": "refresh_token",
-      "refresh_token": token
-    })
-  ).json()
-  const accessToken = data['access_token'] || ''
-  const phone = data["user_name"] || ''
-  const refresh_token = data["refresh_token"] || '已过期'
+  const data = JSON.stringify({
+    "grant_type": "refresh_token",
+    "refresh_token": token
+  })
+  const result = request("https://auth.aliyundrive.com/v2/account/token", data).json()
+  const accessToken = result['access_token'] || ''
+  const phone = result["user_name"] || ''
+  const refresh_token = result["refresh_token"] || '已过期'
+  access_token = accessToken
   return { accessToken, phone, refresh_token }
 }
 
@@ -92,13 +103,11 @@ function getAccessToken(token) {
  *  */
 function signIn(accessToken, row) {
   try {
-    const data = HTTP.post("https://member.aliyundrive.com/v1/activity/sign_in_list",
+    const result = request("https://member.aliyundrive.com/v1/activity/sign_in_list",
       JSON.stringify({ "_rx-s": "mobile" }),
-      { headers: { "Authorization": "Bearer " + accessToken } }
-    )
-    const dataJson = data.json()
-    const signInCount = dataJson['result']['signInCount']
-    const userName = dataJson["user_name"]
+    ).json()
+    const signInCount = result['result']['signInCount']
+    const userName = result["user_name"]
     return {
       row,
       userName,
@@ -113,37 +122,14 @@ function signIn(accessToken, row) {
     }
   }
 }
-
-function signInReward(accessToken, signInCount) {
-  try {
-    const data = HTTP.post(
-      "https://member.aliyundrive.com/v1/activity/sign_in_reward?_rx-s=mobile",
-      JSON.stringify({ "signInDay": signInCount }),
-      { headers: { "Authorization": "Bearer " + accessToken } }
-    )
-    const dataJson = data.json()
-    const name = dataJson["result"]["name"]
-    const description = dataJson["result"]["description"]
-    return {
-      name,
-      description,
-      success: true,
-    }
-  } catch {
-    return {
-      success: false,
-    }
-  }
-}
 /**
  * 获取本日签到奖励
  */
 function signInInfo(accessToken) {
   try {
-    const data = HTTP.post(
+    const data = request(
       "https://member.aliyundrive.com/v2/activity/sign_in_info",
-      JSON.stringify({}),
-      { headers: { "Authorization": "Bearer " + accessToken } }
+      JSON.stringify({})
     )
     const dataJson = data.json()
     const rewards = dataJson["result"]["rewards"]
@@ -160,14 +146,13 @@ function signInInfo(accessToken) {
   }
 }
 /**
- * 获取本日任务奖励
+ * 领取签到奖励
  */
-function signInTaskReward(accessToken, signInCount) {
+function signInReward(accessToken, signInCount) {
   try {
-    const data = HTTP.post(
-      "https://member.aliyundrive.com/v2/activity/sign_in_task_reward",
+    const data = request(
+      "https://member.aliyundrive.com/v1/activity/sign_in_reward?_rx-s=mobile",
       JSON.stringify({ "signInDay": signInCount }),
-      { headers: { "Authorization": "Bearer " + accessToken } }
     )
     const dataJson = data.json()
     const name = dataJson["result"]["name"]
@@ -181,6 +166,26 @@ function signInTaskReward(accessToken, signInCount) {
     return {
       success: false,
     }
+  }
+}
+
+/**
+ * 获取本日任务奖励
+ */
+function signInTaskReward(accessToken, signInCount) {
+  const data = request(
+    "https://member.aliyundrive.com/v2/activity/sign_in_task_reward",
+    JSON.stringify({ "signInDay": signInCount }),
+  )
+  const dataJson = data.json()
+  const name = dataJson.result && dataJson.result.name || ''
+  const description = dataJson.result && dataJson.result.description || ''
+  const message = dataJson["message"]
+  return {
+    name,
+    message,
+    description,
+    success: !!name || !!description,
   }
 }
 
@@ -216,7 +221,7 @@ function main() {
       return
     }
 
-    const rewardResult = signInReward(accessToken, signResult.signInCount)
+    const rewardResult = signInReward(accessToken, signInCount)
     const name = rewardResult.name
     const description = rewardResult.description
     if (!rewardResult.success) {
@@ -230,13 +235,13 @@ function main() {
         sendMailHealper.add(email, `账号：${userName}-签到成功, 本月累计签到${signInCount}天\n本次签到获得${name},${description}`)
       }
     }
-    const signInTaskRewardResult = signInTaskReward(accessToken, signResult.signInCount)
+    const signInTaskRewardResult = signInTaskReward(accessToken, signInCount)
     if (!signInTaskRewardResult.success) {
-      console.log(`第${index + 2}行账号领取任务奖励失败`)
-      sendMailHealper.add(email, `第${index + 2}行账号领取任务奖励失败`)
+      console.log(`第${index + 2}行账号领取任务奖励失败,失败原因：${signInTaskRewardResult.message}`)
+      sendMailHealper.add(email, `第${index + 2}行账号领取任务奖励失败,失败原因：${signInTaskRewardResult.message}`)
     } else {
-      console.log(`第${index + 2}行账号领取任务奖励成功`)
-      sendMailHealper.add(email, `第${index + 2}行账号领取任务奖励成功`)
+      console.log(`第${index + 2}行账号领取任务奖励成功=>${signInTaskRewardResult.description}`)
+      sendMailHealper.add(email, `第${index + 2}行账号领取任务奖励成功=>${signInTaskRewardResult.description}`)
     }
     const signInInfoResult = signInInfo(accessToken)
     if (signInInfoResult.rewards.length) {
